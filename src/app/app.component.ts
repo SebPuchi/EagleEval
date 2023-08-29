b97d98327a938b5a946ca946ea71c77b37af2ab0
Andrew Boessen <boessena@bc.edu>
2023-08-28 21:18:56 -0400

    Express server api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'EagleEval';
+export class AppComponent implements OnInit {
+	title = 'frontEnd';
+	message: any;
+	constructor(private apiService: ApiService) { };
+	ngOnInit() {
+		this.apiService.getMessage().subscribe(data => {
+			this.message = data;
+		});
+	}
 }
