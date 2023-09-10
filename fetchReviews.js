4d369717b5ab228e2b2dccb1d25ab1a4b055744c
Andrew Boessen <boessena@bc.edu>
2023-09-10 14:38:30 -0400

    Add more features and fixed bugs

--- fetchReviews.js
+++ fetchReviews.js
@@ -4,65 +4,45 @@
 
 // Import the 'node-fetch' library for making HTTP requests.
 import fetch from 'node-fetch'
-
-// Function to parse an HTML table element into a JSON array of objects.
-const parseHTMLTableElem = (tableEl) => {
-    // Extract column names from table headers and convert them into an array.
-    const columns = Array.from(tableEl.querySelectorAll('th')).map(it => it.textContent)
-    // Extract table rows from the table body.
-    const rows = tableEl.querySelectorAll('tbody > tr')
-    // Convert the rows and columns into an array of objects (JSON).
-    return Array.from(rows).map(row => {
-        const cells = Array.from(row.querySelectorAll('td'))
-        // Map each cell value to its corresponding column name to create objects.
-        return columns.reduce((obj, col, idx) => {
-            obj[col] = cells[idx].textContent
-            return obj
-        }, {})
-    })
-}
+// Import to convert html table to json
+import HtmlTableToJson from 'html-table-to-json'
 
 // Function to generate the request body for the POST request to Avalanche Blue.
 const genBody = (query) => {
     // Define the request body as a JavaScript object.
-    body = {
-        "strUiCultureIn": "en",
-        "datasourceId": "560",
-        "blockId": "30",
-        "subjectValue": "____[-1]____",
-        "detailValue": "____[-1]____",
-        "gridId": "fbvGrid",
-        "pageActuelle": 1,
-        "strOrderBy": ["col_2", "asc"],
-        "strFilter": ["", query, "ddlFbvColumnSelectorLvl1", ""],
-        // Set pageSize to 1000 to retrieve all data in a single page.
-        "sortCallbackFunc": "__getFbvGrid",
-        "userid": "",
-        "pageSize": "1000"
+    var body = {
+        "strUiCultureIn":"en",
+        "datasourceId":"560",
+        "blockId":"30",
+        "subjectColId":"2",
+        "subjectValue":"____[-1]____",
+        "detailValue":"____[-1]____",
+        "gridId":"fbvGrid","pageActuelle":1,
+        "strOrderBy":["col_2","asc"],
+        "strFilter":["",query,"ddlFbvColumnSelectorLvl1",""],
+        "sortCallbackFunc":"__getFbvGrid",
+        "userid":"","pageSize":"1000"
     }
     // Convert the body object to a JSON string.
     return JSON.stringify(body)
 }
 
 // Async function to fetch reviews from the BC Avalanche Blue API, parse the response, and return review data as JSON.
-const getReviews = async (query) => {
+export const getReviews = async (query) => {
     // Send a POST request to the specified URL with the generated request body and headers.
     const response = await fetch('https://avalanche.bc.edu/BPI/fbview-WebService.asmx/getFbvGrid', {
         method: 'post',
         body: genBody(query),
-        headers: {'Content-Type': 'application/json'}
+        headers: {'Content-Type': 'application/json; charset=UTF-8'}
     });
-
+   
     // Parse the JSON response from the API.
     const rawData = await response.json();
 
     // Extract the HTML table data from the response.
-    htmlTable = rawData["d"][0]
+    const reviews_json = HtmlTableToJson.parse(String(rawData["d"][0]))
 
     // Parse the HTML table data into a JSON array of objects (reviews).
-    return parseHTMLTableElem(htmlTable)
-}
-
-module.exports = {
-    getReviews
+    return reviews_json.results
+    //return parseHTMLTableElem(htmlTable)
 }
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const getReviews = require('fetchReviews.js') 
-const express = require('express');
+import { getReviews } from './fetchReviews.js'
+import 'log-timestamp'
+import express from 'express'
+import bodyParser from 'body-parser'
+import { body, matchedData, validationResult } from 'express-validator'
+
 const app = express();
 
 // handling CORS
@@ -11,6 +15,10 @@ app.use((req, res, next) => {
 	next();
 });
 
+// handle parsing post body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 // route for handling requests from the Angular client
 app.get('/api/message', (req, res) => {
 	res.json({ message:
@@ -18,12 +26,37 @@ app.get('/api/message', (req, res) => {
 });
 
 // route to fetching review data from api for a certain query
-app.put('/api/fetch/reviews', (req, res) => {
-	var query = req.query
-	fetch_response = getReviews(query)
+app.post(
+	'/api/fetch/reviews', 
+	body('fetch_query')
+		.trim()
+		.notEmpty()
+		.escape(),
+	async (req, res) => {
+		const result = validationResult(req)
 
-	res.send(fetch_response)
-})
+		// Validate input has no errors
+		if (result.isEmpty()) {
+			const data = matchedData(req)
+
+			// Query string from body (Class code or professor name)
+			let query = data.fetch_query
+			console.log("Processing response for query: " + query)
+
+			// Wait for response from bc reviews
+			let fetch_response = await getReviews(query)
+
+			console.log("Successfully fetched reviews for: " + data.fetch_query)
+			return res.send(fetch_response)	
+		}
+
+		console.error(
+			req.body.fetch_query ? 
+			"Error for query: " + req.body.fetch_query : "Error: empty fetch query"
+		)
+		res.send('Invalid body params: fetch_query must not be empty')
+	}
+)
 
 app.listen(3000, () => {
 	console.log('Server listening on port 3000');
