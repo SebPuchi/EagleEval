516f04b852beb3eff5161917ab46ea11ed42c4f3
Andrew Boessen <boessena@bc.edu>
2023-09-10 15:45:40 -0400

    Cleaned and filtered json output from api

--- fetchReviews.js
+++ fetchReviews.js
@@ -27,8 +27,52 @@ const genBody = (query) => {
     return JSON.stringify(body)
 }
 
+const cleanKeysAndRemoveNonASCII = (inputArray) => {
+    return inputArray.map((jsonObject) => {
+      const cleanedObject = {};
+      for (const key in jsonObject) {
+        if (jsonObject.hasOwnProperty(key)) {
+          const cleanedKey = key
+            .replace(/[^\x00-\x7F]+/g, '') // Remove non-ASCII characters
+            .replace(/\s+/g, '') // Remove white spaces
+            .toLowerCase(); // Convert to lowercase
+          cleanedObject[cleanedKey] = jsonObject[key];
+        }
+      }
+      return cleanedObject;
+    });
+}
+
+const removeKeysFromArray = (arr, keysToRemove) => {
+    // Use the map method to create a new array with the specified keys removed
+    const newArray = arr.map(obj => {
+      // Create a copy of the original object to avoid modifying it directly
+      const newObj = { ...obj };
+  
+      // Loop through the list of keys to remove
+      keysToRemove.forEach(key => {
+        // Check if the key exists in the object before removing it
+        if (newObj.hasOwnProperty(key)) {
+          delete newObj[key];
+        }
+      });
+  
+      return newObj;
+    });
+  
+    return newArray;
+}
+
 // Async function to fetch reviews from the BC Avalanche Blue API, parse the response, and return review data as JSON.
 export const getReviews = async (query) => {
+    // Data values to exclude in output
+    const uneeded_keys = [
+        "dpt_ins_overall",
+        "dpt_crs_overall",
+        "ratio",
+        "modality effectiveness",
+    ]
+
     // Send a POST request to the specified URL with the generated request body and headers.
     const response = await fetch('https://avalanche.bc.edu/BPI/fbview-WebService.asmx/getFbvGrid', {
         method: 'post',
@@ -42,7 +86,12 @@ export const getReviews = async (query) => {
     // Extract the HTML table data from the response.
     const reviews_json = HtmlTableToJson.parse(String(rawData["d"][0]))
 
-    // Parse the HTML table data into a JSON array of objects (reviews).
-    return reviews_json.results
-    //return parseHTMLTableElem(htmlTable)
+    // Remove uneeded keys in json
+    let json_objects = reviews_json.results[0]
+    let filtered_json = removeKeysFromArray(json_objects, uneeded_keys)
+
+    // Clean JSON keys
+    let result = cleanKeysAndRemoveNonASCII(filtered_json)
+
+    return result
 }
