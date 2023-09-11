b8d20db9a0dc916eb8bec10eb46007917bcc6db0
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2023-09-10 23:18:53 -0400

    Hero Section Components made and Integrated, troubleshooting promlems with scripting / primeng functionality still

---
+++ src/app/hero-section/hero-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-section',
+  templateUrl: './hero-section.component.html',
+  styleUrls: ['./hero-section.component.css']
+})
+export class HeroSectionComponent {
+
+inputValue: string = '';
+
+}
---
+++ src/app/home-bar/home-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-bar',
+  templateUrl: './home-bar.component.html',
+  styleUrls: ['./home-bar.component.css']
+})
+export class HomeBarComponent {
+
+}
