8afce58d0265eefe536e5cadc4ddc34791a92dd3
Andrew Boessen <boessena@bc.edu>
2023-10-17 19:21:44 -0400

    Revert "Split search by type"
    This reverts commit 83f54cf06075b05ef3374012b72960694081506b.

--- src/app/appSettings.ts
+++
@@ -1,3 +0,0 @@
-export class AppSettings {
-  public static API_ENDPOINT = 'http://localhost:3000/api/';
-}
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,34 +1,33 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
-import { AppSettings } from '../appSettings';
+
 
 interface ProfData {
+
   _id: string;
   title: string;
   score: number;
   crs_code: string;
+
 }
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css'],
+  styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent {
-  selectedResult: string = '';
+
+  selectedProf: string = "";
 
   suggestedProfs: string[] = [];
-  seggestedCourses: string[] = [];
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService){ }
 
-  searchProfsorCourse($query: string, route: string) {
-    const url: string = AppSettings.API_ENDPOINT + route;
+ search($event: any) {
+     this.apiService.getProfs($event).subscribe((data: any) => {
+       this.suggestedProfs = data.map((item: any) => item.title);
+     });
+   }
 
-    this.apiService.getSearchResults($query, url).subscribe((data: any) => {
-      return data;
-    });
-  }
-
-  search($event: any) {}
 }
