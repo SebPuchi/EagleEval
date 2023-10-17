83f54cf06075b05ef3374012b72960694081506b
Andrew <boessena@bc.edu>
2023-10-17 18:52:55 -0400

    Split search by type

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -5,18 +5,13 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class ApiService {
-	constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
+  getSearchResults(query: string, url: string) {
+    // Define the data you want to send in the request body (if any)
+    const data = { search_query: query }; // Modify this as per your API's requirements
 
-	getProfs(query: string){
-	   const url = 'http://localhost:3000/api/search';
-
-       // Define the data you want to send in the request body (if any)
-       const data = { search_query: query }; // Modify this as per your API's requirements
-
-       // Send the POST request using the HttpClient's post method
-       return this.http.post(url, data);
-
-	}
+    // Send the POST request using the HttpClient's post method
+    return this.http.post<any>(url, data);
+  }
 }
-
