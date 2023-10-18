e973d674164d02e3b40996b93b1b93d1a91223ef
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2023-10-18 11:43:24 -0400

    routing paths added for prf / class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProfessorPageEntryComponent} from './professor-page-entry/professor-page-entry.component';
+import {ClassrPageEntryComponent} from './classr-page-entry/classr-page-entry.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{ path: 'professors', component: ProfessorPageEntryComponent},
+{ path: 'classes', component: ClassrPageEntryComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
