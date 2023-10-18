609f2f4ac3a6fd4b9596f1f38f96454c1031faeb
Andrew Boessen <boessena@bc.edu>
2023-10-17 20:20:56 -0400

    Autocomplete working

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,9 +7,9 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  getSearchResults(query: string, url: string) {
+  getSearchResults(query: any, url: string) {
     // Define the data you want to send in the request body (if any)
-    const data = { search_query: query }; // Modify this as per your API's requirements
+    const data = { search_query: query.query }; // Modify this as per your API's requirements
 
     // Send the POST request using the HttpClient's post method
     return this.http.post<any>(url, data);
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
 import { AppSettings } from '../appSettings';
+import { forkJoin, map } from 'rxjs';
 
 interface ProfData {
   _id: string;
   title: string;
   score: number;
+}
+
+interface CourseData {
+  _id: string;
+  title: string;
   crs_code: string;
+  score: number;
 }
 
 @Component({
@@ -17,18 +24,39 @@ interface ProfData {
 export class SearchBarComponent {
   selectedResult: string = '';
 
-  suggestedProfs: string[] = [];
-  seggestedCourses: string[] = [];
+  results: any[] = [];
 
   constructor(private apiService: ApiService) {}
 
-  searchProfsorCourse($query: string, route: string) {
-    const url: string = AppSettings.API_ENDPOINT + route;
+  search($event: any) {
+    const profRoute = AppSettings.API_ENDPOINT + 'search/profs';
+    const crsRoute = AppSettings.API_ENDPOINT + 'search/courses';
+    // Run api calls in parallel with combine latest
+    forkJoin({
+      profs: this.apiService.getSearchResults($event, profRoute),
+      courses: this.apiService.getSearchResults($event, crsRoute),
+    })
+      .pipe(
+        map((response) => {
+          // Gets data from response
+          const profs = <Array<any>>response.profs;
+          const courses = <Array<any>>response.courses;
 
-    this.apiService.getSearchResults($query, url).subscribe((data: any) => {
-      return data;
-    });
+          // initialize array for storing combined results
+          const result: any[] = [];
+
+          profs.map((prof: ProfData) => {
+            result.push(prof.title);
+          });
+          courses.map((course: CourseData) => {
+            result.push(course.title + ' - ' + course.crs_code);
+          });
+
+          return result;
+        })
+      )
+      .subscribe((data) => {
+        this.results = data;
+      });
   }
-
-  search($event: any) {}
 }
