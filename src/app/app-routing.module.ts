856083948f8021e07e24e323c6cc0f82a0ad1bce
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2023-10-18 23:25:50 -0400

    home-page added as route (w path) rather than autoloading component upon starting app lifecycle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, } from '@angular/router';
 import {ProfessorPageEntryComponent} from './professor-page-entry/professor-page-entry.component';
 import {ClassrPageEntryComponent} from './classr-page-entry/classr-page-entry.component';
+import {HomePageComponent} from './home-page/home-page.component';
 
 
 const routes: Routes = [
-{ path: 'professors', component: ProfessorPageEntryComponent},
+{ path: 'home', component: HomePageComponent},
+{ path: 'professor', component: ProfessorPageEntryComponent},
 { path: 'classes', component: ClassrPageEntryComponent}
 
 ];
@@ -16,4 +18,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [ProfessorPageEntryComponent,ClassrPageEntryComponent];
+export const routingComponents = [HomePageComponent,ProfessorPageEntryComponent,ClassrPageEntryComponent];
