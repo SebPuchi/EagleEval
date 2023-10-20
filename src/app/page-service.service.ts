37b6255ff9133e936f249f00e8a66a0c2427c7f7
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2023-10-19 20:42:46 -0400

    Routing service functions added to PageService

--- src/app/page-service.service.ts
+++ src/app/page-service.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class PageServiceService {
 
   constructor() { }
+
+  setShowHomePage() {
+      this.showHomePage = true;
+      this.showProfPage = false;
+      this.showClassPage = false;
+    }
+
+    setShowProfPage() {
+      this.showHomePage = false;
+      this.showProfPage = true;
+      this.showClassPage = false;
+    }
+
+    setShowClassPage() {
+      this.showHomePage = false;
+      this.showProfPage = false;
+      this.showClassPage = true;
+    }
+
+
 }
