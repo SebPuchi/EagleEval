fee0b80352029601339974874d359f6268c02bad
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2023-10-29 21:14:38 -0400

    Enthusatic Bar / Avalable outside of class

--- src/app/profComponents/enth-course-material/enth-course-material.component.ts
+++ src/app/profComponents/enth-course-material/enth-course-material.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {ProfPageData, ProfessorService} from "src/app/PageDataService/professor.service";
 
 @Component({
   selector: 'app-enth-course-material',
   templateUrl: './enth-course-material.component.html',
   styleUrls: ['./enth-course-material.component.css']
 })
-export class EnthCourseMaterialComponent {
+export class EnthCourseMaterialComponent  implements OnInit {
 
   enthCourseValue: number = 0;
+ constructor(private prof: ProfessorService) {
+  }
+
+  ngOnInit() {
+
+  this.prof.getProfPageData().subscribe((data: ProfPageData | null) => {
+        if (data) {
+
+          if(data.avgEnthusiastic){
+
+           this.enthCourseValue = data.avgEnthusiastic;
+
+          }
+        }
+
+        //Communciate skeleton
+      })
+
+  }
+
+
 }
