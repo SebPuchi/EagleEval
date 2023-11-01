e25789fb18bc96b833b3e4ab13869d9e33f42541
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2023-11-01 12:32:00 -0400

    services injected correctly

--- src/app/classComponents/class-stat-table/class-stat-table.component.ts
+++ src/app/classComponents/class-stat-table/class-stat-table.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {ProfTableData,ClassService} from "src/app/PageDataService/class.service";
 
 @Component({
   selector: 'app-class-stat-table',
   templateUrl: './class-stat-table.component.html',
   styleUrls: ['./class-stat-table.component.css']
 })
-export class ClassStatTableComponent {
+export class ClassStatTableComponent implements OnInit{
 
   public crsovlandprimeknob: number = 10;
+
+   ngOnInit() {
+   console.log("hellow");
+   }
 
 }
