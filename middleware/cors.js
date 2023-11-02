42aa5aa1978a3fd7bd85c9c1d19c12fb7bec3b3f
Andrew <boessena@bc.edu>
2023-11-02 00:52:08 -0400

    changed api routes

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,6 @@
 // function to handle cors
 export const handleCors = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Origin", "https://www.eagleeval.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
--- src/app/appSettings.ts
+++ src/app/appSettings.ts
@@ -1,4 +1,3 @@
 export class AppSettings {
-  public static API_ENDPOINT =
-    'https://eagle-eval-27556b7530ba.herokuapp.com/api/';
+  public static API_ENDPOINT = 'https://www.eagleeval.com/api/';
 }
