2c0ed795300b45eaf8e041288c11dca34965af3b
Andrew <boessena@bc.edu>
2023-11-02 12:04:10 -0400

    fixes clearing data and route to home

--- src/app/page-service.service.ts
+++ src/app/page-service.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HostListener } from '@angular/core';
+import { ProfessorService } from './PageDataService/professor.service';
+import { ClassService } from './PageDataService/class.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PageServiceService {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private profService: ProfessorService,
+    private classService: ClassService
+  ) {}
   setShowProfPage(id: string) {
+    this.profService.setProfPageData(null);
     this.router.navigate(['professor', id]);
   }
   setShowClassPage(id: string) {
+    this.classService.setCoursePageData(null);
     this.router.navigate(['class', id]);
   }
 }
