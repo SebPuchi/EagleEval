e32c5bec97a80151eb535227ac8b58bd38f1df23
Andrew <boessena@bc.edu>
2023-11-03 11:55:03 -0400

    fix knob

--- src/app/profComponents/score-knob/score-knob.component.ts
+++ src/app/profComponents/score-knob/score-knob.component.ts
@@ -1,39 +1,30 @@
-import { Component,OnInit } from '@angular/core';
-import {ProfPageData, ProfessorService} from "src/app/PageDataService/professor.service";
-
+import { Component, OnInit } from '@angular/core';
+import {
+  ProfPageData,
+  ProfessorService,
+} from 'src/app/PageDataService/professor.service';
 
 @Component({
   selector: 'app-score-knob',
   templateUrl: './score-knob.component.html',
-  styleUrls: ['./score-knob.component.css']
+  styleUrls: ['./score-knob.component.css'],
 })
 export class ScoreKnobComponent implements OnInit {
   value: number | undefined;
-  isDisabled: boolean = false;
 
-  constructor(private prof: ProfessorService) {
-  }
+  constructor(private prof: ProfessorService) {}
 
   ngOnInit() {
-
-  this.prof.getProfPageData().subscribe((data: ProfPageData | null) => {
-        if (data) {
-
-          if(data.avgOverall && data.avgOverall > 0){
-
-           this.value = data.avgOverall;
-
-          }else{
+    this.prof.getProfPageData().subscribe((data: ProfPageData | null) => {
+      if (data) {
+        if (data.avgOverall && data.avgOverall > 0) {
+          this.value = data.avgOverall;
+        } else {
           this.value = NaN;
-          this.isDisabled = true;
-
-          }
         }
+      }
 
-        //Communciate skeleton
-      })
-
+      //Communciate skeleton
+    });
   }
-
-
 }
