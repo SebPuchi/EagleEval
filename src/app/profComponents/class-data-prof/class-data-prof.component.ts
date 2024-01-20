43ce0eedeebdaa2034648244bdaac30615e3b484
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-01-20 17:46:09 -0500

    knob integrated to class table

---
+++ src/app/profComponents/class-data-prof/class-data-prof.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class-data-prof',
+  templateUrl: './class-data-prof.component.html',
+  styleUrls: ['./class-data-prof.component.css']
+})
+export class ClassDataProfComponent {
+
+  courseOvl: number | undefined = 50;
+
+}
