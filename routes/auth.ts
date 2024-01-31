24f94ab5f0e128d164f558776f89f8f69d76d343
andrew <boessena@bc.edu>
2024-01-30 21:31:41 -0500

    add routes for profiles

--- routes/auth.ts
+++ routes/auth.ts
@@ -2,6 +2,9 @@
 import express, { Request, Response } from 'express';
 import passport from 'passport';
 import { ensureAuthenticated } from 'middleware/authentication';
+import { searchById, searchForId } from 'utils/mongoUtils';
+import UserModel from 'models/user';
+import CommentModel from 'models/comment';
 
 // Create an Express router
 const router = express.Router();
@@ -18,14 +21,30 @@ router.get(
   passport.authenticate('google', { failureRedirect: '/' }),
   (req: Request, res: Response) => {
     // Successful authentication, redirect home.
-    res.redirect('/auth/profile');
+    res.redirect('/');
   }
 );
 
-// Define a protected route
-router.get('/profile', ensureAuthenticated, (req: Request, res: Response) => {
-  res.send('User Profile Page');
-});
+// Get users profile data
+router.get(
+  '/profile',
+  ensureAuthenticated,
+  async (req: Request, res: Response) => {
+    const id = (req.query['id'] as any) || null;
+
+    if (id) {
+      if ((req.user as any)._id != id) {
+        return res.status(401).json({ message: 'Not Authorized' });
+      }
+
+      const user_data = await searchById(UserModel, id);
+
+      return res.json(user_data);
+    } else {
+      return res.send('Query string must include id');
+    }
+  }
+);
 
 // Logout route
 router.get('/logout', (req: Request, res: Response, next) => {
@@ -36,5 +55,26 @@ router.get('/logout', (req: Request, res: Response, next) => {
   });
   res.redirect('/');
 });
+
+// Get users comments
+router.get(
+  '/comments',
+  ensureAuthenticated,
+  async (req: Request, res: Response) => {
+    const id = (req.query['id'] as any) || null;
+
+    if (id) {
+      if ((req.user as any)._id != id) {
+        return res.status(401).json({ message: 'Not Authorized' });
+      }
+
+      const user_data = await searchForId(id, CommentModel, 'user_id');
+
+      return res.json(user_data);
+    } else {
+      return res.send('Query string must include id');
+    }
+  }
+);
 
 export { router as auth_router };
