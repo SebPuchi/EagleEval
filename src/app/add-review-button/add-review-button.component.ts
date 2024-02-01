c0268b13ffdc78c366265be97f0e730195e50fe0
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-01-31 22:38:55 -0500

    component added

---
+++ src/app/add-review-button/add-review-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-review-button',
+  templateUrl: './add-review-button.component.html',
+  styleUrls: ['./add-review-button.component.css']
+})
+export class AddReviewButtonComponent {
+
+}
