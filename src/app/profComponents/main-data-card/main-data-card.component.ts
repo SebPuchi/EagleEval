9e6af98452c9563080da6e397d70ebb501f69901
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-02-01 23:10:53 -0500

    score knob functional for prof page

--- src/app/profComponents/main-data-card/main-data-card.component.ts
+++ src/app/profComponents/main-data-card/main-data-card.component.ts
@@ -1,19 +1,40 @@
 import { Component,OnInit } from '@angular/core';
+import {ProfessorService,ProfPageData} from 'src/app/PageDataService/professor.service';
+
 
 @Component({
   selector: 'app-main-data-card',
   templateUrl: './main-data-card.component.html',
   styleUrls: ['./main-data-card.component.css']
 })
-export class MainDataCardComponent {
+export class MainDataCardComponent implements OnInit {
 
-  professorOvl: number | undefined = 50;
+  professorOvl: number | undefined = NaN;
   strokeColor: string = '#6d1f22';
-
   instructorPrepared: number | undefined = 50;
   clearMaterial: number | undefined = 50;
   outsideClass: number | undefined = 50;
   enthusiastic: number | undefined = 50;
+
+
+
+  constructor(private prof: ProfessorService) {}
+
+  ngOnInit() {
+    this.prof.getProfPageData().subscribe((data: ProfPageData | null) => {
+      if (data) {
+        if (data.avgOverall && data.avgOverall > 0) {
+          this.professorOvl = data.avgOverall;
+        } else {
+          this.professorOvl = NaN;
+        }
+      }
+    });
+  }
+
+
+
+
 
 
 
