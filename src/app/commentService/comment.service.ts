d9af6294311de66b8d842dbd013a445aaa705181
andrew <boessena@bc.edu>
2024-02-01 22:12:35 -0500

    comment service

--- src/app/commentService/comment.service.ts
+++ src/app/commentService/comment.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Observable, Subject } from 'rxjs';
+import { AppSettings } from '../appSettings';
+
+const API_ENDPOINT = AppSettings.API_ENDPOINT;
+
+interface Comment {
+  user_id?: string;
+  message: string;
+  createdAt: Date;
+  wouldTakeAgain?: boolean;
+  professor_id: string;
+  course_id?: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentService {
+  constructor(private api: ApiService) {}
 
-  constructor() { }
+  private reviewStatusSubject = new Subject<string>();
+  reviewStatus$: Observable<string> = this.reviewStatusSubject.asObservable();
+
+  createComment(commentData: any): void {
+    const url = API_ENDPOINT + 'comment/prof';
+    this.api.createComment(commentData, url).subscribe({
+      next: (v) => console.log(v),
+      error: (e) => console.error(e),
+      complete: () => console.info('complete'),
+    });
+  }
+
+  deleteComment(id: string): void {
+    const url = API_ENDPOINT + 'comment/prof';
+    this.api.deleteComment(id, url).subscribe({
+      next: (v) => console.log(v),
+      error: (e) => console.error(e),
+      complete: () => console.info('complete'),
+    });
+  }
 }
