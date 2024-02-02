512ac2e0e5784ef3ddde6a117561ebcd1e1513b9
andrew <boessena@bc.edu>
2024-02-01 22:53:02 -0500

    change test timeout

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ export default {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["./src/*"],
+  testTimeout: 20000,
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
