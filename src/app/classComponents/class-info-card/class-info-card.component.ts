fdc33d5ed04f227a3fdbe051dbcbb6eaa8b07bac
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-02-03 15:40:40 -0500

    values init)

--- src/app/classComponents/class-info-card/class-info-card.component.ts
+++ src/app/classComponents/class-info-card/class-info-card.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  ClassService,
+  CoursePageData,
+} from 'src/app/PageDataService/class.service';
 
 @Component({
   selector: 'app-class-info-card',
   templateUrl: './class-info-card.component.html',
   styleUrls: ['./class-info-card.component.css']
 })
-export class ClassInfoCardComponent {
+export class ClassInfoCardComponent implements OnInit{
+
+
+  constructor(private classservice: ClassService) {}
+
+  courseName: string | undefined = undefined;
+  courseCode: string | undefined = undefined;
+  hoursWeek: number | undefined = undefined;
+  description: string | undefined = undefined;
+
+
+  ngOnInit() {
+    this.classservice.getCoursePageData().subscribe((data: CoursePageData | null) => {
+      this.courseName = data?.title || undefined;
+      this.courseCode = data?.crs_code || undefined;
+      this.hoursWeek = data?.avgEffortHours || undefined;
+      this.description = data?.desc || undefined;
+   
+    });
+  }
+
+
+
+
 
 }
