9e4fe16dd156afcc9123d797e4a9020b8123895e
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-02-03 15:16:27 -0500

    class card functional

--- src/app/classComponents/main-data-class/main-data-class.component.ts
+++ src/app/classComponents/main-data-class/main-data-class.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {
+  ClassService,
+  CoursePageData,
+} from 'src/app/PageDataService/class.service';
 
 @Component({
   selector: 'app-main-data-class',
   templateUrl: './main-data-class.component.html',
   styleUrls: ['./main-data-class.component.css']
 })
-export class MainDataClassComponent {
+export class MainDataClassComponent implements OnInit{
 
-  classOvl: number | undefined = 50;
+  classOvl: number | undefined = 0;
   strokeColor: string = '#6d1f22';
+
+  attendanceNecessary: number | undefined = undefined;
+  challenging: number | undefined = undefined;
+  wellOrganized: number | undefined = undefined;
+  helpfulAssignments: number | undefined = undefined;
+
+
+  constructor(private classservice: ClassService) {}
+
+
+
+  ngOnInit() {
+    this.classservice.getCoursePageData().subscribe((data: CoursePageData | null) => {
+      this.classOvl = data?.avgOverall || undefined;
+      this.attendanceNecessary = data?.avgAttendance || undefined;
+      this.challenging = data?.avgChallanging || undefined;
+      this.wellOrganized = data?.avgOriganized || undefined;
+      this.helpfulAssignments = data?.avgAssignments || undefined;
+    });
+  }
+
+
+  
+
+
+
+
 
 }
